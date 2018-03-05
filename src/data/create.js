4726a94bcc76c514a1ca1f58cc07bc2416851110
simon <simon@uwotech.com>
2018-03-06 00:04:07 +0800

    add project

---
+++ src/data/create.js
@@ -0,0 +1,13 @@
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+
+import signReducer from './reducers/sign';
+
+const reducers = {
+  sign: signReducer
+}
+
+const middlewares = [];
+
+let finalCreateStore = applyMiddleware(...middlewares)(createStore);
+
+export default finalCreateStore(reducers);
